c62196ce94d681a2f91da80cc8efe49c7e3dfab5
Banti Kumar <mr.bantikumar9716@gmail.com>
2024-12-24 20:21:51 +0530

    fixed global text component

--- src/Components/CustomText.tsx
+++ src/Components/CustomText.tsx
@@ -1,20 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React,{FC} from 'react'
-import { useTheme } from 'react-native-paper'
+import { StyleSheet, Text, View } from 'react-native';
+import React, { FC, memo } from 'react';
+import { useTheme } from 'react-native-paper';
 import { defaultStyle } from '../Utils';
 import { ICustomText } from './Types';
 
-const CustomText:FC<ICustomText> = ({title,fontSize=15,color,style={},viewStyle={}}) => {
-    const {colors} = useTheme() as any;
+const CustomText: FC<ICustomText> = ({ title, fontSize = 15, color, style, viewStyle = {} }) => {
+  const { colors } = useTheme() as any;
   return (
     <View style={viewStyle}>
-      <Text style={[style,defaultStyle.text,{color:!style.hasOwnProperty('color')?colors.text:color?color:style.color, fontSize:fontSize}]}>{title}</Text>
+      <Text style={[style,defaultStyle.text,{ color: style!?.color || color || colors.text, fontSize: fontSize }]}>{title}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default CustomText
+export default memo(CustomText);
 
 const styles = StyleSheet.create({
-
-})
+  // Add any custom styles here if needed
+});
